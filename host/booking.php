b7b45a13b667c26b35b3efb17654bb7045d138ed
Alex <fajartin.alex@gmail.com>
2024-01-15 16:24:37 +0800

    alex-update

--- host/booking.php
+++ host/booking.php
@@ -18,11 +18,11 @@ $name = $_SESSION['name'];
   
 
 // Fetch booking data for the current host from the database
-$sql = "SELECT b.booking_id, b.property_id, b.check_in_date, b.check_out_date, b.number_of_guests, b.total_price, b.booking_status,
+$sql = "SELECT b.booking_id, b.PROPERTY_ID, b.check_in_date, b.check_out_date, b.number_of_guests, b.total_price, b.booking_status,
         u.user_id, u.email, u.name, p.title, pm.payment_status, pm.amount
         FROM booking_entity b
         JOIN user_entity u ON b.user_id = u.user_id
-        JOIN property_entity p ON b.property_id = p.property_id
+        JOIN property_entity p ON b.PROPERTY_ID = p.PROPERTY_ID
         LEFT JOIN payment_entity pm ON b.booking_id = pm.booking_id
         WHERE p.host_id = $host_id";
 
@@ -63,7 +63,7 @@ $result = mysqli_query($conn, $sql);
 while ($row = $result->fetch_assoc()) {
     echo "<tr>";
     echo "<td>" . $row['booking_id'] . "</td>";
-    echo "<td>" . $row['property_id'] . "</td>";
+    echo "<td>" . $row['PROPERTY_ID'] . "</td>";
     echo "<td>" . $row['check_in_date'] . "</td>";
     echo "<td>" . $row['check_out_date'] . "</td>";
     echo "<td>" . $row['number_of_guests'] . "</td>";
@@ -74,11 +74,12 @@ while ($row = $result->fetch_assoc()) {
 
     echo "<td class='button-action'>";
     if (empty($row['payment_status']) || strpos($row['payment_status'], 'Pending') !== false) {
-        echo "<a href='propertyview.php?id={$row['property_id']}' class='view-button'>View Property <i class='bx bxs-show'></i></a>";
-        echo "<a href='payment.php?booking_id={$row['booking_id']}&property_id={$row['property_id']}&checkin={$row['check_in_date']}&checkout={$row['check_out_date']}&guests={$row['number_of_guest']}&total_price={$row['total_price']}' class='edit-button'>Pay now <i class='bx bxs-message-square-edit'></i></a>";
+        echo "<a href='propertyview.php?id={$row['PROPERTY_ID']}' class='view-button'>View Property <i class='bx bxs-show'></i></a>";
+        echo "<a href='payment.php?booking_id={$row['booking_id']}&property_id={$row['PROPERTY_ID']}&checkin={$row['check_in_date']}&checkout={$row['check_out_date']}&guests={$row['number_of_guest']}&total_price={$row['total_price']}' class='edit-button'>Pay now <i class='bx bxs-message-square-edit'></i></a>";
         echo "<button class='cancel-button' data-booking-id='{$row['booking_id']}' type='button'>Cancel <i class='bx bxs-message-square-x'></i></button>";
     } else {
-        echo "<a href='propertyview.php?id={$row['property_id']}' class='view-button'>View Property <i class='bx bxs-show'></i></a>";
+        
+      echo "<a href='propertyview.php?property_id={$row['PROPERTY_ID']}' class='view-button'>View Property <i class='bx bxs-show'></i></a>";
     }
     echo "</td>";
     
